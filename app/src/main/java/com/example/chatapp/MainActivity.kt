1929b11a8963b93e6f2673e54c3936c76710d977
anuar <anuar.behari@coderclass.nl>
2022-09-16 13:28:35 +0200

    logout button

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +13,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mAuth = FirebaseAuth.getInstance()
+
         userList = ArrayList()
         adapter = UserAdapter(this, userList)
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId ==  R.id.logout){
+            // write the login for the logout
+            mAuth.signOut()
+            finish()
+            return true
+
+        }
+        return true
+    }
+
+
 }
